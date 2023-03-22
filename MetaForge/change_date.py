196ad09ff389f8fd83f95813385d654150b29087
KittenPirate <friesbi7porto@gmail.com>
2023-03-22 14:29:55 +0200

    Allow date-change via reference frame

--- MetaForge/change_date.py
+++ MetaForge/change_date.py
@@ -3,6 +3,9 @@ import argparse
 from MetaForge.paper import Paper
 from MetaForge.misc import Date
 
+from datetime import date
+from datetime import timedelta
+
 if __name__ == "__main__":
     print("Changing date...")
     
@@ -19,6 +22,13 @@ if __name__ == "__main__":
     
     # We now open it with write permissions to clean it first.
     with open(args.latex_file, "w") as f:
-        paper = Paper.set_date(paper, Date.from_DMY(args.publish_date))
+        if args.publish_date.isdigit():
+            # We want to publish with a delay.
+            publication_date = (date.today() + timedelta(days = int(args.publish_date))).strftime("%d-%m-%Y")
+        else:
+            # We want to publish on a specific date.
+            publication_date = args.publish_date
+        
+        paper = Paper.set_date(paper, Date.from_DMY(publication_date))
         f.write(paper)
         f.close()
